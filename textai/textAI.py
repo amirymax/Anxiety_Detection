6b1861178273e21463796f4027481e161f89e008
Siroj Kobilov <92745433+siroj03@users.noreply.github.com>
2023-11-26 18:53:08 +0300

    Add files via upload

---
+++ textai/textAI.py
@@ -0,0 +1,17 @@
+import pickle
+
+class FromText:
+    def __init__(self) -> None:
+        with open('from_text/emoji_from_text_detection_model.pkl', 'rb') as model_file:
+            self.model = pickle.load(model_file)
+    
+    def predict(self, text: str) -> str:
+        state = {0: 'Negative',
+                 2: 'Neutral',
+                 4:'Positive'}
+        n = self.model.predict([text])[0]
+        return state[n]
+
+model = FromText()
+
+print(model.predict(input()))
