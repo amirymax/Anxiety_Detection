982f6d4816124865c238ef445b82b43e1629496b
Zikrullo Amiri <amirymax@gmail.com>
2023-12-10 16:06:03 +0300

    added some files

--- videoai/videoAi.py
+++ videoai/videoAi.py
@@ -1,58 +1,39 @@
 import cv2
-import pickle
+from keras.models import model_from_json
 import numpy as np
-from PIL import Image
+# from keras_preprocessing.image import load_img
+json_file = open("facialemotionmodel.json", "r")
+model_json = json_file.read()
+json_file.close()
+model = model_from_json(model_json)
 
-# Load the trained model
-with open('videoai/video_emotion_detection_model.pkl', 'rb') as file:
-    model = pickle.load(file)
+model.load_weights("facialemotionmodel.h5")
+haar_file=cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+face_cascade=cv2.CascadeClassifier(haar_file)
 
-# Define emotion classes
-emotion_classes = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+def extract_features(image):
+    feature = np.array(image)
+    feature = feature.reshape(1,48,48,1)
+    return feature/255.0
 
-# Load the face cascade
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-# Open a connection to the camera (0 is the default camera)
-cap = cv2.VideoCapture(0)
-
+webcam=cv2.VideoCapture(0)
+labels = {0 : 'angry', 1 : 'disgust', 2 : 'fear', 3 : 'happy', 4 : 'neutral', 5 : 'sad', 6 : 'surprise'}
 while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect faces in the frame
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-
-    for (x, y, w, h) in faces:
-        # Extract the face region from the frame
-        face_roi = gray[y:y + h, x:x + w]
-
-        # Resize the face to the required input size for the model
-        face_roi = cv2.resize(face_roi, (48, 48))
-
-        # Preprocess the face image
-        face_array = np.array(face_roi) / 255.0
-        face_array = np.expand_dims(face_array, axis=0)
-        face_array = np.expand_dims(face_array, axis=-1)
-
-        # Predict emotion using the trained model
-        predictions = model.predict(face_array)
-        emotion_label = emotion_classes[np.argmax(predictions)]
-
-        # Display the emotion label on the frame
-        cv2.putText(frame, emotion_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2, cv2.LINE_AA)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    # Display the resulting frame
-    cv2.imshow('Emotion Detection', frame)
-
-    # Break the loop when 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release the camera and close all windows
-cap.release()
-cv2.destroyAllWindows()
+    i,im=webcam.read()
+    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(im,1.3,5)
+    try: 
+        for (p,q,r,s) in faces:
+            image = gray[q:q+s,p:p+r]
+            cv2.rectangle(im,(p,q),(p+r,q+s),(255,0,0),2)
+            image = cv2.resize(image,(48,48))
+            img = extract_features(image)
+            pred = model.predict(img)
+            prediction_label = labels[pred.argmax()]
+            # print("Predicted Output:", prediction_label)
+            # cv2.putText(im,prediction_label)
+            cv2.putText(im, '% s' %(prediction_label), (p-10, q-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,2, (0,0,255))
+        cv2.imshow("Output",im)
+        cv2.waitKey(27)
+    except cv2.error:
+        pass
